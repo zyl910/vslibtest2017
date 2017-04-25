c8b833032d28d6924a768a1cbd600b7eab74c73f
zyl910 <zyl910@sina.com>
2017-04-25 20:54:42 +0800

    TestUwp: Show uwp environment info.

--- src/LibShared/LibSharedUtil.cs
+++ src/LibShared/LibSharedUtil.cs
@@ -10,6 +10,19 @@ namespace LibShared {
 	public class LibSharedUtil {
 
 		/// <summary>
+		/// 将64位整数转为版本字符串.
+		/// </summary>
+		/// <param name="v">整数值.</param>
+		/// <returns>返回版本字符串.</returns>
+		public static string VersionFromInt(ulong v) {
+			ulong v1 = (v & 0xFFFF000000000000L) >> 48;
+			ulong v2 = (v & 0x0000FFFF00000000L) >> 32;
+			ulong v3 = (v & 0x00000000FFFF0000L) >> 16;
+			ulong v4 = (v & 0x000000000000FFFFL);
+			return string.Format("{0}.{1}.{2}.{3}", v1, v2, v3, v4);
+		}
+
+		/// <summary>
 		/// Append common info (添加公用信息).
 		/// </summary>
 		/// <param name="sb">String buffer (字符串缓冲区).</param>
@@ -17,25 +30,27 @@ namespace LibShared {
 		public static void AppendCommon(StringBuilder sb, string name) {
 			sb.AppendLine(name);
 			sb.AppendLine();
+			// Environment
+			sb.AppendLine("[Environment]");
 #if (!NETFX_CORE)
-			sb.AppendLine(string.Format("Environment.Is64BitOperatingSystem: {0}", Environment.Is64BitOperatingSystem));
-			sb.AppendLine(string.Format("Environment.Is64BitProcess: {0}", Environment.Is64BitProcess));
-			sb.AppendLine(string.Format("Environment.OSVersion: {0}", Environment.OSVersion));
+			sb.AppendLine(string.Format("Is64BitOperatingSystem:\t{0}", Environment.Is64BitOperatingSystem));
+			sb.AppendLine(string.Format("Is64BitProcess:\t{0}", Environment.Is64BitProcess));
+			sb.AppendLine(string.Format("OSVersion:\t{0}", Environment.OSVersion));
 #endif
-			sb.AppendLine(string.Format("ProcessorCount: {0}", Environment.ProcessorCount));
+			sb.AppendLine(string.Format("ProcessorCount:\t{0}", Environment.ProcessorCount));
 #if (!NETFX_CORE)
-			sb.AppendLine(string.Format("Version: {0}", Environment.Version));
+			sb.AppendLine(string.Format("Version:\t{0}", Environment.Version));
 #endif
-			sb.AppendLine(string.Format("typeof(Environment).AssemblyQualifiedName: {0}", typeof(Environment).AssemblyQualifiedName));
+			sb.AppendLine(string.Format("AssemblyQualifiedName:\t{0}", typeof(Environment).AssemblyQualifiedName));
 #if (NETFX_CORE)
 			Assembly assembly = typeof(Environment).GetTypeInfo().Assembly;
 #else
 			Assembly assembly = typeof(Environment).Assembly;
 #endif
-			sb.AppendLine(string.Format("typeof(Environment).Assembly.FullName: {0}", assembly.FullName));
+			sb.AppendLine(string.Format("Assembly.FullName:\t{0}", assembly.FullName));
 #if (!NETFX_CORE)
-			sb.AppendLine(string.Format("typeof(Environment).Assembly.ImageRuntimeVersion: {0}", assembly.ImageRuntimeVersion));
-			sb.AppendLine(string.Format("typeof(Environment).Assembly.Location: {0}", assembly.Location));
+			sb.AppendLine(string.Format("Assembly.ImageRuntimeVersion:\t{0}", assembly.ImageRuntimeVersion));
+			sb.AppendLine(string.Format("Assembly.Location:\t{0}", assembly.Location));
 #endif
 			sb.AppendLine();
 		}
--- src/TestUwp/MainPage.xaml.cs
+++ src/TestUwp/MainPage.xaml.cs
@@ -5,8 +5,11 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.System.Profile;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,17 +20,14 @@ using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
-namespace TestUwp
-{
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+namespace TestUwp {
+	/// <summary>
+	/// 可用于自身或导航至 Frame 内部的空白页。
+	/// </summary>
+	public sealed partial class MainPage : Page {
+		public MainPage() {
+			this.InitializeComponent();
+		}
 
 		private void Page_Loaded(object sender, RoutedEventArgs e) {
 			btnInfo_Click(btnInfo, null);
@@ -36,8 +36,61 @@ namespace TestUwp
 		private void btnInfo_Click(object sender, RoutedEventArgs e) {
 			StringBuilder sb = new StringBuilder();
 			LibSharedUtil.AppendCommon(sb, "TestUwp");
+			AppendInfo(sb);
 			// show.
 			txtOut.Text = sb.ToString();
 		}
+
+		/// <summary>
+		/// 添加信息.
+		/// </summary>
+		/// <param name="sb">String buffer (字符串缓冲区).</param>
+		public static void AppendInfo(StringBuilder sb) {
+			// AnalyticsVersionInfo
+			sb.AppendLine("[AnalyticsVersionInfo]");
+			AnalyticsVersionInfo ai = AnalyticsInfo.VersionInfo;
+			sb.AppendLine(string.Format("DeviceFamily:\t{0}", ai.DeviceFamily));
+			sb.AppendLine(string.Format("DeviceFamilyVersion:\t{0}", ai.DeviceFamilyVersion));
+			sb.AppendLine(string.Format("DeviceFamilyVersion$:\t{0}", LibSharedUtil.VersionFromInt(ulong.Parse(ai.DeviceFamilyVersion)) ));
+			sb.AppendLine();
+			// Package
+			sb.AppendLine("[Package]");
+			Package package = Package.Current;
+			sb.AppendLine(string.Format("Description:\t{0}", package.Description));
+			sb.AppendLine(string.Format("DisplayName:\t{0}", package.DisplayName));
+			//sb.AppendLine(string.Format("InstallDate:\t{0}", package.InstallDate));	// Exception
+			//sb.AppendLine(string.Format("InstalledDate:\t{0}", package.InstalledDate));
+			//sb.AppendLine(string.Format("InstalledLocation:\t{0}", package.InstalledLocation));
+			sb.AppendLine(string.Format("InstalledLocation:\t{0}", package.InstalledLocation.Path));
+			sb.AppendLine(string.Format("PublisherDisplayName:\t{0}", package.PublisherDisplayName));
+			//sb.AppendLine(string.Format("Id:\t{0}", package.Id));
+			sb.AppendLine(string.Format("Id.Architecture:\t{0}", package.Id.Architecture));
+			////sb.AppendLine(string.Format("Id.Author:\t{0}", package.Id.Author)); // System.InvalidCastException:“Unable to cast object of type 'Windows.ApplicationModel.PackageId' to type 'Windows.ApplicationModel.IPackageIdWithMetadata'.”
+			//sb.AppendLine(string.Format("Id.FamilyName:\t{0}", package.Id.FamilyName));
+			//sb.AppendLine(string.Format("Id.FullName:\t{0}", package.Id.FullName));
+			//sb.AppendLine(string.Format("Id.Name:\t{0}", package.Id.Name));
+			////sb.AppendLine(string.Format("Id.ProductId:\t{0}", package.Id.ProductId));   // System.InvalidCastException:“Unable to cast object of type 'Windows.ApplicationModel.PackageId' to type 'Windows.ApplicationModel.IPackageIdWithMetadata'.”
+			//sb.AppendLine(string.Format("Id.Publisher:\t{0}", package.Id.Publisher));
+			//sb.AppendLine(string.Format("Id.PublisherId:\t{0}", package.Id.PublisherId));
+			//sb.AppendLine(string.Format("Id.ResourceId:\t{0}", package.Id.ResourceId));
+			//sb.AppendLine(string.Format("Id.Version:\t{0}", package.Id.Version));
+			PackageVersion pv = package.Id.Version;
+			string pvs = string.Format("{0}.{1}.{2}.{3}", pv.Major, pv.Minor, pv.Revision, pv.Build);
+			sb.AppendLine(string.Format("Id.Version:\t{0}", pvs));
+			sb.AppendLine();
+			// EasClientDeviceInformation
+			sb.AppendLine("[EasClientDeviceInformation]");
+			EasClientDeviceInformation eas = new EasClientDeviceInformation();
+			sb.AppendLine(string.Format("FriendlyName:\t{0}", eas.FriendlyName));
+			sb.AppendLine(string.Format("Id:\t{0}", eas.Id));
+			sb.AppendLine(string.Format("OperatingSystem:\t{0}", eas.OperatingSystem));
+			sb.AppendLine(string.Format("SystemFirmwareVersion:\t{0}", eas.SystemFirmwareVersion));
+			sb.AppendLine(string.Format("SystemHardwareVersion:\t{0}", eas.SystemHardwareVersion));
+			sb.AppendLine(string.Format("SystemManufacturer:\t{0}", eas.SystemManufacturer));
+			sb.AppendLine(string.Format("SystemProductName:\t{0}", eas.SystemProductName));
+			sb.AppendLine(string.Format("SystemSku:\t{0}", eas.SystemSku));
+			sb.AppendLine();
+		}
+
 	}
 }
