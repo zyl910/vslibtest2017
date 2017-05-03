4fe20a4ec348909872f8681980e033c92e9bd2b4
zyl910 <zyl910@sina.com>
2017-05-03 22:23:57 +0800

    LibSharedUtil change to use Reflection for get Environment info.

--- src/LibShared/LibSharedUtil.cs
+++ src/LibShared/LibSharedUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace LibShared {
 	/// <summary>
@@ -127,6 +128,71 @@ namespace LibShared {
 		}
 
 		/// <summary>
+		/// 取得属性值.
+		/// </summary>
+		/// <param name="typ">类型. 为空时自动根据obj来取类型.</param>
+		/// <param name="obj">对象.</param>
+		/// <param name="membername">成员名.</param>
+		/// <param name="ishad">是否存在该属性.</param>
+		/// <returns>返回属性值.</returns>
+		public static object GetPropertyValue(Type typ, object obj, String membername, out bool ishad) {
+			object rt = null;
+			ishad = false;
+			if (null == typ && null == obj) return rt;
+			if (string.IsNullOrEmpty(membername)) return rt;
+			if (null == typ) typ = obj.GetType();
+			// Get Property.
+			PropertyInfo pi = typ.GetRuntimeProperty(membername);
+			if (null == pi) return rt;
+			// Get Value.
+			if (!pi.CanRead) return rt;
+			try {
+				rt = pi.GetValue(obj);
+				ishad = true;
+			}catch(Exception ex) {
+				Debug.WriteLine(ex.ToString());
+			}
+			return rt;
+		}
+
+		/// <summary>
+		/// 输出属性值.
+		/// </summary>
+		/// <param name="sb">String buffer (字符串缓冲区).</param>
+		/// <param name="typ">类型. 为空时自动根据obj来取类型.</param>
+		/// <param name="obj">对象.</param>
+		/// <param name="membername">成员名.</param>
+		/// <param name="formatargidx">属性值的格式化参数索引. 默认为 0.</param>
+		/// <param name="format">格式化串. 默认为 membername + ":\t{0}". </param>
+		/// <param name="args">参数.</param>
+		/// <returns>返回该属性是否成功输出.</returns>
+		public static bool AppendProperty(StringBuilder sb, Type typ, object obj, String membername, int formatargidx = 0, string format=null, params object[] args) {
+			bool rt = false;
+			if (null == sb) return rt;
+			bool ishad = false;
+			object v = GetPropertyValue(typ, obj, membername, out ishad);
+			if (!ishad) return rt;
+			object[] args2 = args;
+			if (null== format) {
+				format = membername + ":\t{0}";
+				args2 = new object[] { v };
+			} else {
+				if (null == args) {
+					args2 = new object[] { };
+				} else {
+					if (formatargidx >= args.Length) {
+						args2 = new object[formatargidx + 1];
+						Array.Copy(args, args2, args.Length);
+					}
+					args2[formatargidx] = v;
+				}
+			}
+			string str = string.Format(format, args2);
+			sb.AppendLine(str);
+			return rt;
+		}
+
+		/// <summary>
 		/// Output common info (输出公用信息).
 		/// </summary>
 		/// <param name="sb">String buffer (字符串缓冲区).</param>
@@ -135,33 +201,46 @@ namespace LibShared {
 			sb.AppendLine(onproject);
 			sb.AppendLine();
 			// Environment
-			sb.AppendLine("[Environment]");
-#if (NETFX_CORE || NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_2 || NETSTANDARD1_3 || NETSTANDARD1_4 || NETSTANDARD1_5 || NETSTANDARD1_6 || NETSTANDARD2_0)
-#else
-			sb.AppendLine(string.Format("Is64BitOperatingSystem:\t{0}", Environment.Is64BitOperatingSystem));
-			sb.AppendLine(string.Format("Is64BitProcess:\t{0}", Environment.Is64BitProcess));
-			sb.AppendLine(string.Format("OSVersion:\t{0}", Environment.OSVersion));
-#endif
-			sb.AppendLine(string.Format("ProcessorCount:\t{0}", Environment.ProcessorCount));
-#if (NETFX_CORE || NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_2 || NETSTANDARD1_3 || NETSTANDARD1_4 || NETSTANDARD1_5 || NETSTANDARD1_6 || NETSTANDARD2_0)
-#else
-			sb.AppendLine(string.Format("Version:\t{0}", Environment.Version));
-#endif
-			sb.AppendLine(string.Format("AssemblyQualifiedName:\t{0}", typeof(Environment).AssemblyQualifiedName));
-#if (NETFX_CORE || NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_2 || NETSTANDARD1_3 || NETSTANDARD1_4 || NETSTANDARD1_5 || NETSTANDARD1_6 || NETSTANDARD2_0)
-			Assembly assembly = typeof(Environment).GetTypeInfo().Assembly;
-#else
-			Assembly assembly = typeof(Environment).Assembly;
-#endif
-			sb.AppendLine(string.Format("Assembly.FullName:\t{0}", assembly.FullName));
-#if (NETFX_CORE || NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_2 || NETSTANDARD1_3 || NETSTANDARD1_4 || NETSTANDARD1_5 || NETSTANDARD1_6 || NETSTANDARD2_0)
-#else
-			sb.AppendLine(string.Format("Assembly.ImageRuntimeVersion:\t{0}", assembly.ImageRuntimeVersion));
-			sb.AppendLine(string.Format("Assembly.Location:\t{0}", assembly.Location));
-#endif
-			sb.AppendLine();
+			OutputEnvironment(sb, onproject);
 			// DefineConstants.
 			OutputDefineConstants(sb, onproject);
+		}
+
+		/// <summary>
+		/// Output Environment (输出环境信息).
+		/// </summary>
+		/// <param name="sb">String buffer (字符串缓冲区).</param>
+		/// <param name="onproject">On project (所处项目)</param>
+		public static void OutputEnvironment(StringBuilder sb, string onproject) {
+			string str = "[Environment]\t# ";
+			if (!string.IsNullOrEmpty(str)) str += " on " + onproject;
+			sb.AppendLine(str);
+			//sb.AppendLine(string.Format("Is64BitOperatingSystem:\t{0}", Environment.Is64BitOperatingSystem));
+			//sb.AppendLine(string.Format("Is64BitProcess:\t{0}", Environment.Is64BitProcess));
+			//sb.AppendLine(string.Format("OSVersion:\t{0}", Environment.OSVersion));
+			//sb.AppendLine(string.Format("ProcessorCount:\t{0}", Environment.ProcessorCount));
+			//sb.AppendLine(string.Format("Version:\t{0}", Environment.Version));
+			Type typ = typeof(Environment);
+			AppendProperty(sb, typ, null, "Is64BitOperatingSystem");
+			AppendProperty(sb, typ, null, "Is64BitProcess");
+			AppendProperty(sb, typ, null, "OSVersion");
+			AppendProperty(sb, typ, null, "ProcessorCount");
+			AppendProperty(sb, typ, null, "Version");
+			sb.AppendLine(string.Format("AssemblyQualifiedName:\t{0}", typeof(Environment).AssemblyQualifiedName));
+			// typeof(Environment).Assembly .
+			sb.AppendLine("[Environment.Assembly]");
+#if (NET20 || NET30 || NET35 || NET40)
+			Assembly assembly = typeof(Environment).Assembly;
+#else
+			Assembly assembly = typeof(Environment).GetTypeInfo().Assembly;
+#endif
+			//sb.AppendLine(string.Format("Assembly.FullName:\t{0}", assembly.FullName));
+			//sb.AppendLine(string.Format("Assembly.ImageRuntimeVersion:\t{0}", assembly.ImageRuntimeVersion));
+			//sb.AppendLine(string.Format("Assembly.Location:\t{0}", assembly.Location));
+			AppendProperty(sb, null, assembly, "FullName");
+			AppendProperty(sb, null, assembly, "ImageRuntimeVersion");
+			AppendProperty(sb, null, assembly, "Location");
+			sb.AppendLine();
 		}
 
 		/// <summary>
